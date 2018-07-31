709103e18aebac4b9c4de2881649d0e99b47f274
David Celentano <davidwcelentano@gmail.com>
2018-07-31 12:05:01 -0400

    project updates + small analytics fix

--- Truth/AppDelegate.swift
+++ Truth/AppDelegate.swift
@@ -27,10 +27,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         if let dict = myDict {
             secretKey = dict.value(forKey: "Flurry_key") as? String
-            Flurry.startSession(secretKey, with: FlurrySessionBuilder
-                .init()
-                .withCrashReporting(true)
-                .withLogLevel(FlurryLogLevelAll))
+            if let secretKey = secretKey {
+                Flurry.startSession(secretKey, with: FlurrySessionBuilder
+                  .init()
+                  .withCrashReporting(true)
+                  .withLogLevel(FlurryLogLevelAll))
+            }
+            else {
+                assertionFailure("NO FLURRY KEY")
+            }
         }
         
         // MARK: Rating popup
